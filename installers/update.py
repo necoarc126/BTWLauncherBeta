ce6a3cc38f101ef47aefadd60f8cb8b3901bb47f
Neco Arc <135509022+necoarc126@users.noreply.github.com>
2024-06-05 11:17:34 +0300

    добавил функционал
    теперь прога оповещает об успешном (и не успешном) обновлении

--- installers/update.py
+++ installers/update.py
@@ -1,13 +1,21 @@
 import os
-import shutil
 import zipfile
 import subprocess
-from pathlib import Path
-import subprocess
+import ctypes
 
 archive_path = os.path.join(os.getcwd(), "btwupdate.zip")
 output_folder = "C:\\BTWLauncher"
 
-with zipfile.ZipFile(archive_path, 'r') as zip_ref:
-    zip_ref.extractall(output_folder)
-    subprocess.Popen(r"C:\BTWLauncher\app\app.exe")
+def show_message(title, message):
+    ctypes.windll.user32.MessageBoxW(0, message, title, 0x40 | 0x0)
+
+def update_and_launch():
+    try:
+        with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+            zip_ref.extractall(output_folder)
+        show_message("Обновление завершено", "Обновление завершено!")
+        subprocess.Popen([r"C:\BTWLauncher\app\app.exe"], shell=True)
+    except Exception as e:
+        show_message("Ошибка", f"Произошла ошибка: {str(e)}")
+
+update_and_launch()
