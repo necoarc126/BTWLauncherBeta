f0ebde1bec6d80826cc3a10be9563e45aadd6259
Neco Arc <135509022+necoarc126@users.noreply.github.com>
2024-06-05 11:02:19 +0300

    исправил путь распаковки

--- installers/install.py
+++ installers/install.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import subprocess
 
 archive_path = os.path.join(os.getcwd(), "btwlaunch.zip")
-output_folder = "C:\\BTWLauncher"
+output_folder = "C:\\"
 
 with zipfile.ZipFile(archive_path, 'r') as zip_ref:
     zip_ref.extractall(output_folder)
